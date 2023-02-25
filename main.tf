502c23c01da78019abe17469264ca0668734543e
VENUCHANAPATHIGIT <96813823+VENUCHANAPATHIGIT@users.noreply.github.com>
2023-02-25 09:20:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,3 +110,20 @@ resource "aws_route_table_association" "associate-public-subnet-2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.my-public-RT.id
 }
+
+# Create elastic ip for NAT GW
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  tags = {
+    Name = "nat_eip"
+  }
+}
+
+# Create NAT GW
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "my-3tier-NAT"
+  }
+}
