89f790e30f0aa78be8c55f79ebaecc2ac43cd99f
VENUCHANAPATHIGIT <96813823+VENUCHANAPATHIGIT@users.noreply.github.com>
2023-02-25 08:11:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "vpc_cidr_block" {
   description = "The cidr block for my 3 tier vpc"
   default     = "10.0.0.0/16"
 }
+
+variable "pub1_3tier_cidr_block" {
+  description = "The cidr block for my pub1_3tier"
+  default     = "10.10.1.0/24"
+}
+
+variable "pub1_3tier_zone" {
+  description = "The zone for my pub1_3tier"
+  default     = "us-east-1a"
+}
