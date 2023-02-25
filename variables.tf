9382fd8b71e6dfb64cf5044552148188ac9c9b63
VENUCHANAPATHIGIT <96813823+VENUCHANAPATHIGIT@users.noreply.github.com>
2023-02-25 08:50:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,12 +8,12 @@ variable "vpc_cidr_block" {
   default     = "10.10.0.0/16"
 }
 
-variable "pub1_3tier_zonea" {
+variable "sub_3tier_zonea" {
   description = "The zonea for my pub1_3tier"
   default     = "us-east-2a"
 }
 
-variable "pub1_3tier_zoneb" {
+variable "sub_3tier_zoneb" {
   description = "The zoneb for my pub1_3tier"
   default     = "us-east-2b"
 }
