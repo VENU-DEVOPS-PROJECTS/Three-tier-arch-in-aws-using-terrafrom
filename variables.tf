4a210c5b88f7fd3c2de6723b5131e0900491330d
VENUCHANAPATHIGIT <96813823+VENUCHANAPATHIGIT@users.noreply.github.com>
2023-02-25 08:49:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,11 +8,6 @@ variable "vpc_cidr_block" {
   default     = "10.10.0.0/16"
 }
 
-variable "pub1_3tier_cidr_block" {
-  description = "The cidr block for my pub1_3tier"
-  default     = "10.10.1.0/24"
-}
-
 variable "pub1_3tier_zonea" {
   description = "The zonea for my pub1_3tier"
   default     = "us-east-2a"
@@ -21,4 +16,34 @@ variable "pub1_3tier_zonea" {
 variable "pub1_3tier_zoneb" {
   description = "The zoneb for my pub1_3tier"
   default     = "us-east-2b"
+}
+
+variable "pub1_3tier_cidr_block" {
+  description = "The cidr block for my pub1_3tier"
+  default     = "10.10.1.0/24"
+}
+
+variable "pub2_3tier_cidr_block" {
+  description = "The cidr block for my pub2_3tier"
+  default     = "10.10.2.0/24"
+}
+
+variable "priv3_3tier_cidr_block" {
+  description = "The cidr block for my priv3_3tier"
+  default     = "10.10.3.0/24"
+}
+
+variable "priv4_3tier_cidr_block" {
+  description = "The cidr block for my priv4_3tier"
+  default     = "10.10.4.0/24"
+}
+
+variable "priv5_3tier_cidr_block" {
+  description = "The cidr block for my priv5_3tier"
+  default     = "10.10.5.0/24"
+}
+
+variable "priv6_3tier_cidr_block" {
+  description = "The cidr block for my priv6_3tier"
+  default     = "10.10.6.0/24"
 }
