bac7c91737871e9a037737609526f4d6771d30f1
VENUCHANAPATHIGIT <96813823+VENUCHANAPATHIGIT@users.noreply.github.com>
2023-02-25 08:51:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,12 +21,24 @@ resource "aws_internet_gateway" "threetier-arch-igw" {
 }
 
 # Create a public subnet 1 with CIDR block 10.0.1.0/24
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.threetier-arch-vpc.id
   cidr_block = "${var.pub1_3tier_cidr_block}"
-  availability_zone = "${var.pub1_3tier_zonea}"
+  availability_zone = "${var.sub_3tier_zonea}"
   tags = {
     Name = "public_subnet_1"
+  }
+  # Specify that this is a public subnet
+  map_public_ip_on_launch = true
+}
+
+# Create a public subnet 2 with CIDR block 10.0.2.0/24
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.threetier-arch-vpc.id
+  cidr_block = "${var.pub2_3tier_cidr_block}"
+  availability_zone = "${var.sub_3tier_zoneb}"
+  tags = {
+    Name = "public_subnet_2"
   }
   # Specify that this is a public subnet
   map_public_ip_on_launch = true
